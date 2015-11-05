f33dafbebb031e74756ea34b589b012ccf10f4b5
Sylwester Madej <sylwek.madej@gmail.com>
2015-11-05 14:43:37 +0100

    Retrofit

--- app/src/main/java/com/adaptris/tumblrviewer/PostListFragment.java
+++ app/src/main/java/com/adaptris/tumblrviewer/PostListFragment.java
@@ -5,13 +5,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 
+import retrofit.Callback;
+import retrofit.RestAdapter;
+import retrofit.RetrofitError;
+import retrofit.client.*;
+import retrofit.client.Response;
+
 /**
  * Created by Sylwester on 2015-11-05.
  */
 public class PostListFragment extends ListFragment {
 
     public static final String BLOG = "blog";
-    public static final String URL = "http://api.tumblr.com
+    public static final String URL = "http://api.tumblr.com";
+    private ArrayAdapter<Post> adapter;
 
     public static PostListFragment newInstance(String blog) {
         PostListFragment postListFragment = new PostListFragment();
@@ -21,11 +28,34 @@ public class PostListFragment extends ListFragment {
         return postListFragment;
     }
 
+
+
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        setListAdapter(new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1));
-        setEmptyText(getArguments().getString(BLOG));
+        adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1);
+        setListAdapter(adapter);
+        String blog = getArguments().getString(BLOG);
+
+
+        RestAdapter.Builder builder = new RestAdapter.Builder();
+        builder.setEndpoint(URL);
+        builder.setLogLevel(RestAdapter.LogLevel.FULL);
+        TumblrService tumblrService = builder.build().create(TumblrService.class);
+        tumblrService.getPosts(blog, new Callback<TumblrResponse>() {
+            @Override
+            public void success(TumblrResponse tumblrResponse, Response response) {
+                adapter.addAll(tumblrResponse.getResponse().getPosts());
+
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+
+            }
+        });
+
+
     }
 
 
--- app/src/main/java/com/adaptris/tumblrviewer/TumblrService.java
+++ app/src/main/java/com/adaptris/tumblrviewer/TumblrService.java
@@ -1,12 +1,14 @@
 package com.adaptris.tumblrviewer;
 
+import retrofit.Callback;
 import retrofit.http.GET;
+import retrofit.http.Path;
 
 /**
  * Created by Sylwester on 2015-11-05.
  */
 public interface TumblrService {
 
-    @GET("/v2/blog/wehavethemunchies.tumblr.com/posts?api_key=fD0HOvNDa2z10uyozPZNnjeb4fEFGVGm58zttH6cXSe4K0qC64&limit=20offset=0")
-    void getPosts();
+    @GET("/v2/blog/{blog}.tumblr.com/posts?api_key=fD0HOvNDa2z10uyozPZNnjeb4fEFGVGm58zttH6cXSe4K0qC64&limit=20offset=0")
+    void getPosts(@Path("blog") String blog, Callback<TumblrResponse> callback);
 }
